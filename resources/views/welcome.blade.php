a6625625568333e2141c302061d818584c481aae
Taha Karami <tahakarami2088@gamil.com>
2026-01-01 14:58:22 +0330

    feat: add admin-panel themeplate to project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,5 @@
     class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 <h1>Eduport</h1>
 @livewireScripts
-<livewire:post.create/>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::get('/admin' , [\App\Http\Controllers\Admin\PanelController::class, 'index'])->name('panel');
